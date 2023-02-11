df6cab6bb9ecb49dde38346ca66706e5e4398d29
Adria Martinez <adriamartinez94@gmail.com>
2023-02-11 14:00:16 +0100

    Install chakra ui and init basic theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/global.css'
-import type { AppProps } from 'next/app'
+import "@/styles/global.css";
+import theme from "@/styles/theme";
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
