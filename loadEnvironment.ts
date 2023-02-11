f0564eb648fd586fe666f27f8c3563e0fff34099
Adria Martinez <adriamartinez94@gmail.com>
2023-02-11 22:08:11 +0100

    Fix loadEnvironments

--- loadEnvironment.ts
+++ loadEnvironment.ts
@@ -1,11 +1,7 @@
 import { Environments } from "types/types";
 
-const environments = {} as Environments;
-
-if (process.env) {
-  const { NEXT_PUBLIC_BASE_URL } = process.env;
-
-  environments.baseUrl = NEXT_PUBLIC_BASE_URL!;
-}
+const environments = {
+  baseUrl: process.env.NEXT_PUBLIC_BASE_URL!,
+} as Environments;
 
 export default environments;
