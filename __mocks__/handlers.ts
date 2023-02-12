a47f8e08118bb937573af0f2e4b23c26c0661add
Adria Martinez <adriamartinez94@gmail.com>
2023-02-12 13:32:49 +0100

    Add test of useUniversities

--- __mocks__/handlers.ts
+++ __mocks__/handlers.ts
@@ -1,2 +1,27 @@
+import { getUniversities } from "@factories/universityFactory";
 import { rest } from "msw";
-export const handlers = [];
+import endpoints from "routes/apiEndpoints";
+import { UniversityResponse } from "types/types";
+
+const { listOfUniversities } = endpoints;
+export const handlers = [
+  rest.get(listOfUniversities, (req, res, ctx) => {
+    const limit = req.url.searchParams.get("pagination[pageSize]");
+
+    const universities = getUniversities(Number(limit));
+
+    const response: UniversityResponse = {
+      data: universities,
+      meta: {
+        pagination: {
+          page: 1,
+          pageSize: Number(limit),
+          total: universities.length,
+          pageCount: Number(limit),
+        },
+      },
+    };
+
+    return res(ctx.json(response));
+  }),
+];
