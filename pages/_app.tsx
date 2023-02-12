648772f4ed117f268b9724b73b4dddaab0825921
Adria Martinez <adriamartinez94@gmail.com>
2023-02-12 15:05:57 +0100

    Implement providers of react query on app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,21 @@ import "@/styles/global.css";
 import theme from "@/styles/theme";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { useState } from "react";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <QueryClientProvider client={queryClient}>
+        <Layout>
+          <Hydrate state={pageProps.dehydratedState}>
+            <Component {...pageProps} />
+          </Hydrate>
+        </Layout>
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
