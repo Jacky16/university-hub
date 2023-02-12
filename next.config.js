20feb617ea14c9b940fe98dc37a20d113757cba9
Adria Martinez <adriamartinez94@gmail.com>
2023-02-12 19:44:10 +0100

    Add university detail component with tests

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  images: {
+    domains: ["cdn.wuolahservices.com"],
+    disableStaticImages: true,
+    minimumCacheTTL: 0,
+  },
+};
