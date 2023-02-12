eca99ac4f16d2e80f35eb2ee4346816c7100cdbe
Adria Martinez <adriamartinez94@gmail.com>
2023-02-12 16:30:00 +0100

    Add list of univerties with infinity scroll on index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,61 @@
+import HeadLayout from "@/components/HeadLayout";
+import UniversityCardList from "@/components/UniversityCardList";
+import {
+  Box,
+  Flex,
+  Heading,
+  Spinner,
+  StackDivider,
+  VStack,
+} from "@chakra-ui/react";
+import useInfinityUniversities from "hooks/useInfinityUniversities";
+import InfiniteScroll from "react-infinite-scroll-component";
+
 export default function Home() {
-  return <h1>Hello Next-JS</h1>;
+  const { universities, fetchNextPage, isLoading, hasNextPage } =
+    useInfinityUniversities(100);
+  return (
+    <>
+      <HeadLayout
+        headData={{
+          title: "Home",
+          description: "Home page",
+        }}
+      />
+      <VStack
+        direction={"column"}
+        spacing={10}
+        divider={<StackDivider />}
+        borderColor="gray.200"
+        alignItems={"center"}
+      >
+        <Heading alignSelf={"start"}>Universities</Heading>
+
+        <Box>
+          <InfiniteScroll
+            dataLength={universities.length}
+            next={fetchNextPage}
+            hasMore={hasNextPage || isLoading}
+            loader={
+              <Flex
+                justifyContent={"center"}
+                alignItems={"center"}
+                paddingY={"10"}
+              >
+                <Spinner
+                  thickness="4px"
+                  speed="0.65s"
+                  emptyColor="gray.200"
+                  color="blue.500"
+                  size="xl"
+                />
+              </Flex>
+            }
+          >
+            <UniversityCardList universities={universities} />
+          </InfiniteScroll>
+        </Box>
+      </VStack>
+    </>
+  );
 }
