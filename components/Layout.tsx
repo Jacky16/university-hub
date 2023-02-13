b5e3f166db9d4241f669241ee0206fac4e813e5c
Adria Martinez <adriamartinez94@gmail.com>
2023-02-13 16:57:54 +0100

    Implement Header on Layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,18 @@
 import { Container } from "@chakra-ui/react";
 import React from "react";
+import Header from "./Header";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => (
-  <Container as={"main"} maxWidth={"8xl"} lang={"en"}>
-    {children}
-  </Container>
+  <>
+    <Header />
+    <Container as={"main"} maxWidth={"8xl"} lang={"en"}>
+      {children}
+    </Container>
+  </>
 );
 
 export default Layout;
