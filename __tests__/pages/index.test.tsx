281dae48bbefea29a9e5a8c67cead27c0d3672e2
Adria Martinez <adriamartinez94@gmail.com>
2023-02-13 18:10:54 +0100

    Add test of getStaticProps on index page

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -2,19 +2,23 @@ import HomePage from "@/pages/index";
 import { render, screen } from "@testing-library/react";
 import RenderWrapper from "mocks/RenderWrapper";
 import { HomePageData } from "types/types";
+import { getStaticProps } from "../../pages/index";
+import mockFetch from "jest-fetch-mock";
+
+mockFetch.enableMocks();
+
+const dataHomePage: HomePageData = {
+  title: "University Hub",
+  description: "The best site to find your FAVORITE UNIVERSITY",
+  card: {
+    title: "Find your university",
+    description:
+      "Sure that you find the university that you are looking for. Trust us 😄",
+    universitiesHref: "/universities",
+  },
+};
 
 describe("Given the index page", () => {
-  const dataHomePage: HomePageData = {
-    title: "University Hub",
-    description: "The best site to find your FAVORITE UNIVERSITY",
-    card: {
-      title: "Find your university",
-      description:
-        "Sure that you find the university that you are looking for. Trust us 😄",
-      universitiesHref: "/universities",
-    },
-  };
-
   describe("When is rendered with Home page Data", () => {
     test("Then it should show a title called 'The best site to find your FAVORITE UNIVERSITY'", () => {
       const firstTitle = "The best site to find your";
@@ -46,5 +50,23 @@ describe("Given the index page", () => {
     expect(link).toBeInTheDocument();
 
     expect(link).toHaveAttribute("href", expectedHref);
+  });
+});
+
+describe("Given the function getStaticProps", () => {
+  beforeEach(() => {
+    mockFetch.resetMocks();
+  });
+
+  test("Then it should return the data", async () => {
+    const expectedData = {
+      props: {
+        data: dataHomePage,
+      },
+    };
+
+    const result = await getStaticProps();
+
+    expect(result).toEqual(expectedData);
   });
 });
